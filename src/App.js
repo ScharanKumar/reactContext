7ba18df052d967789782b8bad4277fbcfe1c29d7
ScharanKumar <9848charan@gmail.com>
2024-07-22 17:08:36 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header'
+import LandingSection from './components/LandingSection'
+import FeaturesSection from './components/FeaturesSection'
+
+class App extends Component {
+  state = {activeLanguage: 'EN'}
+
+  changeLanguage = activeLanguage => {
+    this.setState({activeLanguage})
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <LandingSection />
+        <FeaturesSection />
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
